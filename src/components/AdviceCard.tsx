698558ac77ea715d2cce382a8d4f79e412bc291e
Israel Sant'Anna <israel.santanna@gmail.com>
2022-06-19 12:49:13 -0300

    feat(components): update AdviceCard
     - update styles
     - conditionally render Loading, Slip or SlipError
     - render Divider
     - pass props to NewAdviceButton

--- src/components/AdviceCard.tsx
+++ src/components/AdviceCard.tsx
@@ -1,9 +1,32 @@
+import { useEffect } from "react";
+import { useAdvice } from "../hooks/useSlipAdvice"
 import { NewAdviceButton } from "./NewAdviceButton"
+import { Slip } from "./Slip";
+import { LoadingAdvice } from "./LoadingAdvice";
+import { Divider } from "./Divider";
+import { SlipError } from "./SlipError";
+
+const ADVICE_SLIP_API_URL = 'https://api.adviceslip.com/advice'
 
 export const AdviceCard = () => {
+  const {slip, loading, error, getNewAdvice} = useAdvice(ADVICE_SLIP_API_URL);
+
   return (
-    <div>
-      <NewAdviceButton />
+    <div className="
+      bg-darkGrayishBlue
+      grid
+      justify-items-center
+      text-center
+      rounded-lg
+      m-4
+    ">
+      {
+        loading
+        ? <LoadingAdvice />
+        : (error && <SlipError error={ error } />) || (slip && <Slip slip={ slip } />)
+      }
+      <Divider />
+      <NewAdviceButton disabled={ loading} onClick={ getNewAdvice } />
     </div>
   )
 }
