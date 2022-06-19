145c718353f90dabe6091ce9eb343fe629edb9b7
Israel Sant'Anna <israel.santanna@gmail.com>
2022-06-19 12:42:59 -0300

    feat(components): create SlipError
     - show Error type
     - show Error text

---
+++ src/components/SlipError.tsx
@@ -0,0 +1,20 @@
+import { SlipError as SlipErrorType } from "../types/advice.types"
+
+export const SlipError = ({ error }: {error: SlipErrorType}) => {
+  return (
+    <div className="p-4 max-w-screen-mobile">
+      <div className={`
+        ${['notice', 'warning'].includes(error.type) ? 'text-neonYellow' :  'text-neonRed'}
+        text-sm
+        tracking-[0.4em]
+        mt-6
+        mb-4
+      `}>
+        {error.type.toUpperCase()}
+      </div>
+      <span className="text-lightCyan text-quote">
+        {error.text}
+      </span>
+    </div>
+  );
+}
