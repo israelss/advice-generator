9ef7378711ac0e5df58eb0a568e96ef56f3914be
Israel Sant'Anna <israel.santanna@gmail.com>
2022-06-19 12:46:15 -0300

    feat(components): update NewAdviceButton
     - include attribute onClick, received by props
     - include attribute disabled, received by props
     - update styles

--- src/components/NewAdviceButton.tsx
+++ src/components/NewAdviceButton.tsx
@@ -1,9 +1,25 @@
 import diceIcon from '../assets/images/icon-dice.svg'
 
-export const NewAdviceButton = () => {
+export const NewAdviceButton = ({ disabled, onClick }: { disabled: boolean, onClick: VoidFunction }) => {
   return (
-    <div className='bg-neonGreen rounded-full p-3 w-10'>
-      <img className='w-full' src={diceIcon} alt="Button to get new advice, green with a black dice inside" />
-    </div>
+    <button
+      onClick={ onClick }
+      disabled={ disabled }
+      className='
+        bg-neonGreen
+        rounded-full
+        p-5
+        relative
+        -bottom-8
+        active:drop-shadow-neonGreen
+        disabled:bg-grayishBlue
+        disabled:filter-none
+      '>
+      <img
+        className='w-full'
+        src={ diceIcon }
+        alt="Button to get new advice, green with a black dice inside"
+      />
+    </button>
   )
 }
